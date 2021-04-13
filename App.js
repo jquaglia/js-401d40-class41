8dd153ae255ae62e738bfc1e96d88769026ea7f0
jquaglia <jqugali@gmail.com>
2021-04-12 22:04:28 -0700

    got everything up and connected. Started to play around with native

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, FlatList, Linking } from 'react-native';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+// expo requires that we install these via the command line
+import * as Permissions from 'expo-permissions';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+
+  const [contacts, setContacts] = useState([]);
+  const [permissions, setPermissions] = useState(false);
+
+  const getPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+    setPermissions(true);
+  }
+
+  const showContacts = async () => {
+    const contactList = await Contacts.getContactsAsync();
+    console.log(contactList);
+    setContacts(contactList.data);
+  }
+
+  const call = (contact) => {
+    let phoneNumber = contact.phoneNumbers[0].number.replace(/[\(\)\-\s+]/g, '');
+    let link = `tel:${phoneNumber}`;
+    Linking.canOpenURL(link)
+      .then(() => Linking.openURL(link));
+  }
+
+  useEffect(() => {
+    // if (permissions) {
+    //   showContacts();
+    // } else {
+    getPermissions();
+    // }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.safeArea} />
+      <SafeAreaView>
+        <Button
+          onPress={showContacts}
+          title="Press to see contacts"
+        />
+        <SafeAreaView>
+          <Text>Contacts</Text>
+          <FlatList
+            data={contacts}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <Button title={item.name} onPress={() => call(item)} />}
+          />
+        </SafeAreaView>
+        {/* <Text style={styles.text}>New Stuff!</Text>
+        <Text style={styles.largerText}>More text</Text> */}
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 0,
+    backgroundColor: '#FF5236'
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // cocktail recipe, 
+    // paddingTop: 50,
+    borderWidth: 5,
+    borderColor: "red",
+    backgroundColor: 'bisque',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    flex: 2,
+    backgroundColor: "pink",
+  },
+  largerText: {
+    flex: 3,
+    backgroundColor: 'blue'
+  }
 });
